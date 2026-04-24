9f649c1e03338058078bbbc3b23da9f584dd42e3
maria <mariaahmad88@icloud.com>
2026-04-24 14:47:07 +0200

    legg til Maria-komponent for gruppemedlem

--- src/components/Maria.jsx
+++ src/components/Maria.jsx
@@ -1,1 +1,9 @@
-    
+    import './Maria.css'
+
+const member = {
+  name: 'Maria Fullt Navn',
+  email: 'din.epost@hiof.no',
+  study: 'Informatikk – Bachelor',
+  imageUrl: 'https://api.dicebear.com/9.x/shapes/svg?seed=maria12',
+  imageAlt: 'Illustrasjon for Maria',
+}
